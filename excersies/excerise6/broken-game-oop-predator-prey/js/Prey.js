bc1c0f96ced0d68929e9c997a9ac85dbdbcc60ad
lisa <lisadubuc119@gmail.xom>
2019-10-24 14:23:59 -0400

    ex6
    syntax fix

--- excersies/excerise6/broken-game-oop-predator-prey/js/Prey.js
+++ excersies/excerise6/broken-game-oop-predator-prey/js/Prey.js
@@ -14,7 +14,8 @@ class Prey {
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  constructor(x, why, speed, fillColor, radius) {
+  //fixed
+  constructor(x, y, speed, fillColor, radius) {
     // Position
     this.x = x;
     this.y = y;
