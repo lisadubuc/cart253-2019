dc3b2537f7cbe43274e906c17476314cb370f664
lisa <lisadubuc119@gmail.xom>
2019-10-24 14:25:26 -0400

    ex 6
    syntax fixed

--- excersies/excerise6/broken-game-oop-predator-prey/js/Prey.js
+++ excersies/excerise6/broken-game-oop-predator-prey/js/Prey.js
@@ -85,7 +85,8 @@ class Prey {
       noStroke();
       fill(this.fillColor);
       this.radius = this.health;
-      ellipse(this.x, this.y, this.radius * "two");
+      //fixed
+      ellipse(this.x, this.y, this.radius * 2);
       pop();
     }
 
